3a9798f6eb249461928c4f1f73e11413d977af61
AarushJalla <aarushjallalearnings@gmail.com>
2026-03-24 18:32:14 +0000

    Add files via upload

---
+++ Activity 2_variables_20260324.py
@@ -0,0 +1,14 @@
+Birthday1="4.8.2015"
+print("My birthday is on",Birthday1)
+
+Birthday2="21.8.2011"
+print("My sister's birthday is on",Birthday2)
+
+Birthday3="6.4.1987"
+print("My mother's birthday is on",Birthday3)
+
+Birthday4="26.12.1983"
+print("My dad's birthday is on",Birthday4)
+
+Birthday5="8.3.2014"
+print("My friend's birthday is on",Birthday5)
